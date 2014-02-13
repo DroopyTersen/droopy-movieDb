2bbcb4f94af509b58b870a396d32394f6176244f
unknown <apetersen@LT0191.skyline.gb>
2014-02-12 23:34:45 -0600

    initial commit

---
+++ MovieDbService.js
@@ -0,0 +1,45 @@
+var HttpService = require("droopy-http"),
+	format = require("util").format,
+	q = require("q");
+
+var MovieDbService = function (apiKey) {
+	this.baseUrl = config.movieDb.baseUrl;
+	this.apiKey = apiKey;
+};
+
+MovieDbService.prototype = new HttpService();
+
+MovieDbService.prototype.search = function(searchText) {
+	var url = format("%s/search/movie?query=%s&api_key=%s", this.baseUrl, searchText, this.apiKey);
+	return this.getJSON(url);
+};
+
+MovieDbService.prototype.searchForOne = function(searchText) {
+	var deferred = q.defer(),
+		self = this;
+	this.search(searchText).then(function(response){
+		if (response.results.length > 0 ) {
+			self.getFullMovie(response.results[0].id).then(deferred.resolve);
+		} else {
+			deferred.reject("NO RESULTS!! - " + searchText);
+		}
+	}).fail(deferred.reject);
+
+	return deferred.promise;
+
+};
+
+MovieDbService.prototype.getFullMovie = function(id) {
+	var appendToResponse = "append_to_response=casts,keywords,trailers,releases";
+	var url = format("%s/movie/%s?api_key=%s&%s", this.baseUrl, id, this.apiKey, appendToResponse);
+	return this.getJSON(url);
+};
+
+MovieDbService.prototype.getPerson = function(id) {
+	var appendToResponse = "append_to_response=credits";
+	var url = format("%s/person/%s?api_key=%s&%s", this.baseUrl, id, this.apiKey, appendToResponse);
+	console.log(url);
+	return this.getJSON(url);
+};
+
+module.exports = MovieDbService;
